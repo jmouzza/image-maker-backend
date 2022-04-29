c858af5029eb86849e5ad5066fef2a46f2c77726
Jesus Mouzza <ing.jmouzza@gmail.com>
2022-04-29 06:00:59 -0400

    ajuste en index

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ var app = require("./app");
 var port = 3900;
 
 /* mongoose.Promise = global.Promise; */
-mongoose.connect("mongodb+srv://admin:admin@cluster0.j5hgm.mongodb.net/test",{ useNewUrlParser : true })
+app.listen(port, ()=>{
+    console.log("Servidor corriendo en http://localhost:"+port);
+})
+/* mongoose.connect("mongodb+srv://admin:admin@cluster0.j5hgm.mongodb.net/test",{ useNewUrlParser : true })
 .then(()=>{
     console.log("Conexion establecida");
     //Crear Servidor y recibir http requests
-    app.listen(port, ()=>{
-        console.log("Servidor corriendo en http://localhost:"+port);
-    })
     
 })
-.catch((err)=>console.log(err));
-
-module.exports = mongoose;
+.catch((err)=>console.log(err)); */

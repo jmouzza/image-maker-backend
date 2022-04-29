2e42d3a64f2439c8c323826344e7250889a760cc
Jesus Mouzza <ing.jmouzza@gmail.com>
2022-04-29 05:32:29 -0400

    ajuste en index

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var mongoose = require("mongoose");
 var app = require("./app");
 var port = 3900;
 
-mongoose.Promise = global.Promise;
+/* mongoose.Promise = global.Promise; */
 mongoose.connect("mongodb+srv://admin:admin@cluster0.j5hgm.mongodb.net/test",{ useNewUrlParser : true })
 .then(()=>{
     console.log("Conexion establecida");
